5ef3f2154e71376138b33c6547c1690760ded675
Jeff Lin <thejefflin@gmail.com>
2022-04-10 21:21:46 -0400

    commit tree 2725afeb6ed11b3b9735b109cc866b9c4684e45f

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+ }
+ 
+ function destructivelyPrependCat(name) {
+     cats.unshift(name);
+ }
+ 
+ function  destructivelyRemoveLastCat() {
+     cats.pop();
+ }
+ 
+ function destructivelyRemoveFirstCat() {
+     cats.shift();
+ }
+
+ function appendCat(name) {
+    let copyOfCats = [...cats]
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    let copyOfCats = [...cats]
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+
+function removeLastCat(name) {
+    let copyOfCats = [...cats]
+    copyOfCats.pop(name);
+    return copyOfCats;
+}
+
+function removeFirstCat(name) {
+    let copyOfCats = [...cats]
+    copyOfCats.shift(name);
+    return copyOfCats;
+}
